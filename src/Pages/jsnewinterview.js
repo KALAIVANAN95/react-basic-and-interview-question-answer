81848f86104dc9fbd049fb37be7ceb08628b079b
KALAIVANAN95 <101535641+KALAIVANAN95@users.noreply.github.com>
2026-05-27 11:24:17 +0530

    Update jsnewinterview.js

--- src/Pages/jsnewinterview.js
+++ src/Pages/jsnewinterview.js
@@ -12,3 +12,20 @@ function addTwoSum(arr,target){
 console.log(addTwoSum([2,4,5,6,7,1],9))
 
 ===============================================================================================================
+
+function duplicateFind(arr){
+    for(let i=0;i<arr.length;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[i]===arr[j]){
+                return arr[i]
+            }
+        }
+    }
+}
+
+console.log(duplicateFind([1,2,2,2,2,3,5]))
+
+const arr=[1,22,22,2,4,5,5]
+const res=[...new Set(arr)]
+console.log(res)
+
