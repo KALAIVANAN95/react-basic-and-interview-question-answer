ae8465878bb1891d968af6c5e2c51580f94edfd1
KALAIVANAN95 <101535641+KALAIVANAN95@users.noreply.github.com>
2026-05-27 11:42:46 +0530

    Implement removeDuplicate function in jsnewinterview.js
    Add function to remove duplicates from an array.

--- src/Pages/jsnewinterview.js
+++ src/Pages/jsnewinterview.js
@@ -27,5 +27,26 @@ console.log(duplicateFind([1,2,2,2,2,3,5]))
 
 const arr=[1,22,22,2,4,5,5]
 const res=[...new Set(arr)]
+===============================================================================================================
+    function removeDupilcate(arr){
+    let result=[]
+    
+    for(let i=0;i<arr.length;i++){
+        
+        let found=false
+        
+        for(let j=0;j<result.length;j++){
+            if(arr[i]===result[j]){
+                found=true
+            }
+        }
+        if(!found){
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+
+console.log(removeDupilcate([1,2,2,3,4,5,5]))
 console.log(res)
 
