76fc28b025eec3e1beec53cb6738e310052b45d5
KALAIVANAN95 <101535641+KALAIVANAN95@users.noreply.github.com>
2026-06-14 10:36:38 +0530

    Add multiple utility functions in jsnewinterview.js

--- src/Pages/jsnewinterview.js
+++ src/Pages/jsnewinterview.js
@@ -72,6 +72,64 @@ function counterLength(str){
 }
 console.log(counterLength("kalai"))
     ==================================
+
+    function largestNumber(arr){
+    let bigNo=arr[0]
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>bigNo){
+            bigNo=arr[i]
+        }
+    }
+    console.log(bigNo)
+    return bigNo
+}
+largestNumber([20,23,204,234,2343])
+
+function removeDublicate(arr){
+    let result=[...new Set(arr)]
+    console.log(result)
+    return result
+}
+removeDublicate([2,3,3,45,6,65,42,2])
+
+function countChar(str,target){
+    let newVal=0
+    for(let i=0;i<str.length;i++){
+        if(str[i]===target){
+            newVal++
+        }
+    }
+    console.log(newVal)
+    return newVal
+}
+countChar("KALAI","A")
+
+
+function sumValue(arr){
+    let sum=0
+    for(let i=0;i<arr.length;i++){
+        sum=sum+arr[i]
+    }
+    console.log(sum)
+    return sum
+}
+sumValue([2,3,3])
+
+function evenNu(arr){
+    let newVals=[]
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]%2===0){
+            newVals.push(arr[i])
+        }
+    }
+    console.log(newVals)
+    return newVals
+    
+}
+evenNu([1,2,3,4,5,6,6,8,10])
+
+
+
 function addTwoSum(arr,target){
     for(let i=0;i<arr.length;i++){
         for(let j=i+1;j<arr.length;j++){
